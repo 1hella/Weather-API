459614112cf57adb762c2f62a23bc55b32cd1201
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-02-27 00:27:05 -0800

    Add new routes

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 stations_df = pd.read_csv('data_small/stations.txt', skiprows=17)
 stations_df = stations_df[['STAID', 'STANAME                                 ']]
+
+
 @app.route("/")
 def home():
     return render_template("home.html", data=stations_df.to_html())
@@ -19,6 +21,23 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df["    DATE"].astype(str)
+    result = df.loc[df['    DATE'].str.startswith(year)].to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
