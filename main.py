df19e53e5717a3e1bdb8ace4fdacebbc51ba220a
Stephen Wanhella <Stephen.wanhella@gmail.com>
2023-05-22 15:54:43 -0700

    convert main to use a database and populate it on first run.

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from flask import Flask, render_template
 import pandas as pd
-import os
+import sqlite3
+
+from migrate_data import populate_database
 
 app = Flask(__name__)
-
 DATA_FOLDER = "data_small"
 
-stations_df = pd.read_csv(f'{os.path.dirname(__file__)}/data/stations.txt', skiprows=17)
-stations_df = stations_df[['STAID', 'STANAME                                 ']]
+connection = sqlite3.connect("data.db")
+stations_df = pd.read_sql("SELECT * FROM stations", con=connection)
 
 
 @app.route("/")
 def home():
-    return render_template("home.html", data=stations_df.to_html())
+    return render_template("home.html", data=stations_df.to_html(index=False))
 
 
 @app.route("/api/v1/<station>/<date>")
-def about(station, date):
-    filename = f"{os.path.dirname(__file__)}/{DATA_FOLDER}/TG_STAID{str(station).zfill(6)}.txt"
-    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
-    row = df.loc[df['    DATE'] == date]
-    temperature = row['   TG'].squeeze() / 10
+def temperature(station, date):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    station = int(station)
+    query = f"SELECT tg FROM temperatures WHERE staid=({station}) AND date=('{date}')"
+    cursor.execute(query)
+    row = cursor.fetchone()
+    if row is not None:
+        temperature = row[0] / 10
+    else:
+        temperature = None
     return {"station": station,
             "date": date,
             "temperature": temperature}
@@ -28,20 +35,32 @@ def about(station, date):
 
 @app.route("/api/v1/<station>")
 def all_data(station):
-    filename = f"{os.path.dirname(__file__)}/{DATA_FOLDER}/TG_STAID{str(station).zfill(6)}.txt"
-    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
-    result = df.to_dict(orient="records")
-    return result
+    connection = sqlite3.connect('data.db')
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    station = int(station)
+    query = f"SELECT * FROM temperatures WHERE staid=({station})"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    return [dict(station=row['staid'], date=row["date"], temperature=row["tg"] / 10) for row in rows]
 
 
 @app.route("/api/v1/yearly/<station>/<year>")
 def yearly(station, year):
-    filename = f"{os.path.dirname(__file__)}/{DATA_FOLDER}/TG_STAID{str(station).zfill(6)}.txt"
-    df = pd.read_csv(filename, skiprows=20)
-    df['    DATE'] = df["    DATE"].astype(str)
-    result = df.loc[df['    DATE'].str.startswith(year)].to_dict(orient="records")
-    return result
+    connection = sqlite3.connect('data.db')
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    station = int(station)
+    query = f"SELECT * FROM temperatures WHERE staid=({station}) AND date LIKE ('{year}-__-__')"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    return [dict(station=row['staid'], date=row["date"], temperature=row["tg"] / 10) for row in rows]
 
+
+sample_query = temperature(10, "1992-10-08")
+if sample_query['temperature'] is None:
+    print("populating database...")
+    populate_database()
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
--- migrate_data.py
+++ migrate_data.py
@@ -2,30 +2,19 @@ from glob import glob
 import sqlite3
 import pandas as pd
 
-connection = sqlite3.connect("data.db")
-cursor = connection.cursor()
-try:
-    cursor.execute("""
-    CREATE TABLE "temperatures" (
-        "staid"	INTEGER,
-        "souid"	INTEGER,
-        "date"	TEXT,
-        "tg"	INTEGER,
-        "q_tg"	INTEGER,
-        CONSTRAINT primary_key PRIMARY KEY (staid, date)
-    )""")
-    connection.commit()
-except sqlite3.OperationalError as e:
-    print(e)
 
-filenames = glob("data_small/TG_STAID*.txt")
+def populate_database():
+    connection = sqlite3.connect("data.db")
 
-for filename in filenames:
-    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
-    df.rename(columns={" SOUID": "souid", "STAID": "staid", "    DATE": "date", "   TG": "tg", " Q_TG": "q_tg"},
-              inplace=True)
-    print(filename)
-    try:
-        df.to_sql("temperatures", con=connection, if_exists="append", index=False)
-    except sqlite3.IntegrityError as e:
-        print(e)
+    filenames = glob("data_small/TG_STAID*.txt")
+
+    for filename in filenames:
+        df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+        df.rename(columns={" SOUID": "souid", "STAID": "staid", "    DATE": "date", "   TG": "tg", " Q_TG": "q_tg"},
+                  inplace=True)
+        df['date'] = df['date'].astype(str)
+        print("populating", filename)
+        try:
+            df.to_sql("temperatures", con=connection, if_exists="append", index=False)
+        except sqlite3.IntegrityError as e:
+            print(e)
