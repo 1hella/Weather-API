3fb8fb2ec762cc0fab9eff7e0ed51907f4b46bb2
Stephen Wanhella <Stephen.wanhella@gmail.com>
2023-05-22 12:02:03 -0700

    migrate csv files into a database
    migrate csv files into a database
    
    remove data.db

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 
 app = Flask(__name__)
 
+DATA_FOLDER = "data_small"
+
 stations_df = pd.read_csv(f'{os.path.dirname(__file__)}/data/stations.txt', skiprows=17)
 stations_df = stations_df[['STAID', 'STANAME                                 ']]
 
@@ -15,7 +17,7 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    filename = f"{os.path.dirname(__file__)}/data/TG_STAID{str(station).zfill(6)}.txt"
+    filename = f"{os.path.dirname(__file__)}/{DATA_FOLDER}/TG_STAID{str(station).zfill(6)}.txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
     row = df.loc[df['    DATE'] == date]
     temperature = row['   TG'].squeeze() / 10
@@ -26,7 +28,7 @@ def about(station, date):
 
 @app.route("/api/v1/<station>")
 def all_data(station):
-    filename = f"{os.path.dirname(__file__)}/data/TG_STAID{str(station).zfill(6)}.txt"
+    filename = f"{os.path.dirname(__file__)}/{DATA_FOLDER}/TG_STAID{str(station).zfill(6)}.txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
     result = df.to_dict(orient="records")
     return result
@@ -34,7 +36,7 @@ def all_data(station):
 
 @app.route("/api/v1/yearly/<station>/<year>")
 def yearly(station, year):
-    filename = f"{os.path.dirname(__file__)}/data/TG_STAID{str(station).zfill(6)}.txt"
+    filename = f"{os.path.dirname(__file__)}/{DATA_FOLDER}/TG_STAID{str(station).zfill(6)}.txt"
     df = pd.read_csv(filename, skiprows=20)
     df['    DATE'] = df["    DATE"].astype(str)
     result = df.loc[df['    DATE'].str.startswith(year)].to_dict(orient="records")
---
+++ migrate_stations.py
@@ -0,0 +1,21 @@
+import sqlite3
+import pandas as pd
+from main import DATA_FOLDER
+import os
+
+# must create data.db
+connection = sqlite3.connect("data.db")
+
+# cursor = connection.cursor()
+# cursor.execute("DELETE FROM temperatures")
+# cursor.execute("DELETE FROM stations")
+# connection.commit()
+
+stations_df = pd.read_csv(f'{os.path.dirname(__file__)}/{DATA_FOLDER}/stations.txt', skiprows=17)
+stations_df = stations_df[['STAID', 'STANAME                                 ']]
+stations_df.rename(columns={"STAID": "staid", "STANAME                                 ": "staname"}, inplace=True)
+
+if DATA_FOLDER == "data_small":
+    stations_df = stations_df.loc[stations_df["staid"] <= 100]
+
+stations_df.to_sql('stations', con=connection, index=False, if_exists="replace")
